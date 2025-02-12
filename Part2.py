86d0d30a77626d6df428a7ca026b3aa54d47bb9b
ZHOU sebastien <sebastien.4084@gmail.com>
2025-02-12 16:16:04 +0100

    Add count_attacks function and unit tests for attack detection

--- Part2.py
+++ Part2.py
@@ -15,6 +15,15 @@ def is_attacking(pos1, pos2):
         
     return False
 
+def count_attacks(queens):
+    attacks = {i: 0 for i in range(len(queens))}
+    for i in range(len(queens)):
+        for j in range(i + 1, len(queens)):
+            if is_attacking(queens[i], queens[j]):
+                attacks[i] += 1
+                attacks[j] += 1
+    return attacks
+
 class TestSingleAttackQueens(unittest.TestCase):
 
     def test_init_board(self):
@@ -42,6 +51,28 @@ class TestSingleAttackQueens(unittest.TestCase):
         ]
         for pos1, pos2 in attack_positions:
             self.assertTrue(is_attacking(pos1, pos2))
+
+    def test_count_attacks_single(self):
+        # Test avec deux reines qui s'attaquent
+        queens = [(0, 0), (0, 2)]
+        attacks = count_attacks(queens)
+        self.assertEqual(attacks[0], 1)
+        self.assertEqual(attacks[1], 1)
+
+    def test_count_attacks_multiple(self):
+        # Test avec quatre reines
+        queens = [(0, 0), (1, 1), (2, 2), (3, 3)]
+        attacks = count_attacks(queens)
+        # Chaque reine attaque toutes les autres (diagonale)
+        for i in range(4):
+            self.assertEqual(attacks[i], 3)
+
+    def test_count_attacks_no_attacks(self):
+        # Test avec des reines qui ne s'attaquent pas
+        queens = [(0, 0), (1, 2), (2, 4), (4, 1)]
+        attacks = count_attacks(queens)
+        for count in attacks.values():
+            self.assertEqual(count, 0)
 
 if __name__ == '__main__':
     unittest.main()
